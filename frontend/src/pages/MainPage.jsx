0fd90700bf49db30c32aa791fdb3c6234fa478dc
Barani velan k <baranivelan007@gmail.com>
2025-12-30 13:56:01 +0530

    username div change 2

--- frontend/src/pages/MainPage.jsx
+++ frontend/src/pages/MainPage.jsx
@@ -129,7 +129,7 @@ export default function MainPage() {
                 </div>
             </div>
             { /* name of user */}
-            <div className="p-4 border-b border-slate-700">
+            <div className="p-4 border-b border-slate-700 align-center">
                 <h2 className="text-lg">Welcome, <span className="font-semibold text-slate-200">{username}</span></h2>
             </div>
             {/* Popup form */}
